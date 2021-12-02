cfca7f637fc59e107e3821b73255ab7b0d9e59c1
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-01 18:55:13 -0600

    added stretch work

--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,49 @@
 const fs = require('fs');
-// const readline = require('readline');
+const readline = require('readline');
 const request = require('request');
 const args = process.argv.slice(2);
 const link = args[0];
 const path = args[1];
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 request(link, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
+  if (error === null) {
+    fs.exists(path, (bool) => {
+      if (bool) {
+        rl.question("File already exists. Overwrite? Y/N ", answer => {
+          if (answer.toLowerCase() === 'y') {
+            write(path, body);
+            rl.close();
+          } else {
+            process.exit();
+          }
+        });
+      } else {
+        write(path, body);
+        rl.close();
+      }
+    });
+  } else if (error.code === 'ENOTFOUND') {
+    console.log(`Invalid link: ${error.hostname}`);
+    process.exit();
+  }
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  fs.writeFile(path, body, err => {
+});
+
+const write = function(mypath, mybody) {
+  fs.writeFile(mypath, mybody, err => {
     if (err) {
-      console.error(err);
+      if (err.code === 'ENOENT') {
+        console.log(`${err.path} does not exist.`);
+      } else {
+        console.error(err);
+      }
       return;
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+    console.log(`Downloaded and saved ${mybody.length} bytes to ${mypath}`);
   });
-});
+};
